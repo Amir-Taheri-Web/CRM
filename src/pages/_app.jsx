bc3a2db1c012511f0af839cfca18e0f488d2eae2
Amir Taheri <amir.taheri.web@protonmail.com>
2024-04-02 12:06:58 +0330

    favicon added

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,13 @@
 import "@/styles/globals.css";
 
+import { DM_Sans } from "next/font/google";
+
+const dm_sans = DM_Sans({ subsets: ["latin"] });
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={dm_sans.className}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
