94580540b5ba9cb3f63966656389928b7d3f2a44
Amir Taheri <amir.taheri.web@protonmail.com>
2024-04-03 18:09:30 +0330

    Products Inputs functionality added and its style updated

--- src/components/modules/ProductsInputs.jsx
+++ src/components/modules/ProductsInputs.jsx
@@ -1,46 +1,76 @@
 import styles from "@/styles/ProductsInputs.module.css";
+import React from "react";
 
 const ProductsInputs = ({ form, setForm }) => {
   const { products } = form;
 
-  const changeHandler = () => {};
+  const changeHandler = (e, i) => {
+    const { name, value } = e.target;
+
+    const newProducts = products.map((product, index) => {
+      if (index === i) product[name] = value;
+
+      return product;
+    });
+
+    setForm((form) => ({ ...form, products: newProducts }));
+  };
+
+  const addProductHandler = () => {
+    setForm((form) => ({
+      ...form,
+      products: [...products, { productName: "", price: "", qty: "" }],
+    }));
+  };
 
   return (
     <div className={styles.container}>
       <h3>Products</h3>
       <div className={styles.inputs}>
-        <div>
-          <label htmlFor="productName">Product Name</label>
-          <input
-            type="text"
-            value={products.productName}
-            name="productName"
-            onChange={changeHandler}
-            id="productName"
-          />
-        </div>
+        {products.map((product, index) => (
+          <div key={index} className={styles.inputsWrapper}>
+            <div>
+              <label htmlFor="productName">Product Name</label>
+              <input
+                type="text"
+                value={product.productName}
+                name="productName"
+                onChange={(e) => changeHandler(e, index)}
+                id="productName"
+              />
+            </div>
 
-        <div>
-          <label htmlFor="price">Price</label>
-          <input
-            type="text"
-            value={products.price}
-            name="price"
-            onChange={changeHandler}
-            id="price"
-          />
-        </div>
+            <div>
+              <label htmlFor="price">Price</label>
+              <input
+                type="number"
+                value={product.price}
+                name="price"
+                onChange={(e) => changeHandler(e, index)}
+                id="price"
+              />
+            </div>
 
-        <div>
-          <label htmlFor="qty">Quantity</label>
-          <input
-            type="text"
-            value={products.qty}
-            name="qty"
-            onChange={changeHandler}
-            id="qty"
-          />
-        </div>
+            <div>
+              <label htmlFor="qty">Quantity</label>
+              <input
+                type="number"
+                value={product.qty}
+                name="qty"
+                onChange={(e) => changeHandler(e, index)}
+                id="qty"
+              />
+            </div>
+          </div>
+        ))}
+
+        <button
+          type="button"
+          className={styles.addProductButton}
+          onClick={addProductHandler}
+        >
+          Add Product
+        </button>
       </div>
     </div>
   );
