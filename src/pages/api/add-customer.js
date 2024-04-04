18dc4e09eaa067d43231eb2a6b9d8c13ddcbdb7f
Amir Taheri <amir.taheri.web@protonmail.com>
2024-04-04 18:12:26 +0330

    api updated

--- src/pages/api/add-customer.js
+++ src/pages/api/add-customer.js
@@ -1,6 +1,8 @@
 import Customer from "@/model/customer";
 import connectDB from "@/utils/connectDB";
 
+const EMAIL_VALIDATION = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
 const handler = async (req, res) => {
   if (req.method === "POST") {
     await connectDB(res);
@@ -12,6 +14,31 @@ const handler = async (req, res) => {
         code: 422,
         status: "failure",
         message: "Please fill all the required fields",
+      });
+    }
+
+    if (data.firstName.length < 2 || data.lastName.lastName < 2) {
+      res.status(422).json({
+        code: 422,
+        status: "failure",
+        message: "First Name and Last Name must be at least 2 characters",
+      });
+    }
+
+    if (data.phone.length !== 11) {
+      res.status(422).json({
+        code: 422,
+        status: "failure",
+        message:
+          "Phone number must be 11 characters and start with 0 like 09198882233",
+      });
+    }
+
+    if (!EMAIL_VALIDATION.test(data.email)) {
+      res.status(422).json({
+        code: 422,
+        status: "failure",
+        message: "Email is not valid",
       });
     }
 
--- src/pages/api/customer/edit/[customerId].js
+++ src/pages/api/customer/edit/[customerId].js
@@ -2,12 +2,47 @@ import INPUTS from "@/constants/inputs";
 import Customer from "@/model/customer";
 import connectDB from "@/utils/connectDB";
 
+const EMAIL_VALIDATION = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
 const handler = async (req, res) => {
   if (req.method === "PATCH") {
     await connectDB(res);
 
     const id = req.query.customerId;
     const data = req.body.data;
+
+    if (!data.firstName || !data.lastName || !data.email) {
+      res.status(422).json({
+        code: 422,
+        status: "failure",
+        message: "Please fill all the required fields",
+      });
+    }
+
+    if (data.firstName.length < 2 || data.lastName.lastName < 2) {
+      res.status(422).json({
+        code: 422,
+        status: "failure",
+        message: "First Name and Last Name must be at least 2 characters",
+      });
+    }
+
+    if (data.phone.length !== 11) {
+      res.status(422).json({
+        code: 422,
+        status: "failure",
+        message:
+          "Phone number must be 11 characters and start with 0 like 09198882233",
+      });
+    }
+
+    if (!EMAIL_VALIDATION.test(data.email)) {
+      res.status(422).json({
+        code: 422,
+        status: "failure",
+        message: "Email is not valid",
+      });
+    }
 
     try {
       const customer = await Customer.findById(id);
